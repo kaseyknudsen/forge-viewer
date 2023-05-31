85ead333384d3f0540cdab4f857c541c85c3d9e2
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-05-30 18:52:42 -0700

    added script and style files

---
+++ script.js
@@ -0,0 +1,4 @@
+const viewer = document.querySelector("#viewer");
+const models = document.querySelector("#models");
+viewer.style.border = "5px solid red";
+models.style.border = "5px solid green";
